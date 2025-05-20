2af019ecd90c74156d777bdded23ab0fb0bf0e6b
abdelrahman shrief ali <abdo.shrief270@gmail.com>
2025-05-20 03:18:20 +0300

    Update Api to add Versioning

--- routes/api.php
+++ routes/api.php
@@ -5,35 +5,24 @@ use App\Http\Controllers\ExamController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Traits\ApiResponse;
-Route::get('/', function () {
-    return response(['message' => 'Hello world!']);
-});
 
-
-Route::prefix('exams')->group(function () {
-    Route::get('/', [ExamController::class, 'index']);
-});
-
-Route::prefix('auth')->group(function (){
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::prefix('username')->group(function () {
-        Route::post('/check', [AuthController::class, 'checkUserName']);
+Route::prefix('v1')->name('api.v1.')->group(function () {
+    Route::get('/', function () {
+        return response(['message' => 'Hello world!']);
     });
 
 
-//    Route::prefix('parents')->group(function (){
-//        Route::post('/login', [AuthController::class, 'login']);
-//    });
-//    Route::prefix('schools')->group(function (){
-//        Route::post('/login', [AuthController::class, 'login']);
-//    });
-//    Route::prefix('students')->group(function (){
-//        Route::post('/login', [AuthController::class, 'login']);
-//    });
-//    Route::prefix('students')->group(function (){
-//        Route::post('/login', [AuthController::class, 'login']);
-//    });
+    Route::prefix('exams')->group(function () {
+        Route::get('/', [ExamController::class, 'index']);
+    });
+
+    Route::prefix('auth')->group(function () {
+        Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/login', [AuthController::class, 'login']);
+        Route::prefix('username')->group(function () {
+            Route::post('/check', [AuthController::class, 'checkUserName']);
+        });
+    });
 });
 
 
