6a334a1b12a3336ddcb63377a448157c64ea8640
abdelrahman shrief ali <abdo.shrief270@gmail.com>
2025-05-20 03:04:21 +0300

    Update Schools Actions in Dashboard

--- app/Filament/Resources/SchoolResource/Pages/EditSchool.php
+++ app/Filament/Resources/SchoolResource/Pages/EditSchool.php
@@ -5,15 +5,30 @@ namespace App\Filament\Resources\SchoolResource\Pages;
 use App\Filament\Resources\SchoolResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Hash;
 
 class EditSchool extends EditRecord
 {
     protected static string $resource = SchoolResource::class;
 
-    protected function getHeaderActions(): array
+    protected function mutateFormDataBeforeFill(array $data): array
     {
-        return [
-            Actions\DeleteAction::make(),
-        ];
+        $data['password'] = null;
+        return $data;
+    }
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        if($data['password']==null){
+            unset($data['password']);
+        }
+        if(isset($data['password']))
+        {
+            $data['password'] = Hash::make($data['password']);
+        }
+        return $data;
+    }
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
     }
 }
