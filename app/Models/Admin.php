19465a59d9fe442cbe6091111201530ec0fcac48
abdelrahman shrief ali <abdo.shrief270@gmail.com>
2025-08-08 19:20:27 +0300

    Phase 2.52

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,12 +2,19 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Permission\Traits\HasRoles;
 
-class Admin extends Authenticatable
+class Admin extends Authenticatable implements FilamentUser
 {
     use HasRoles;
     protected $fillable =['name','email','password'];
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->active;
+    }
 }
