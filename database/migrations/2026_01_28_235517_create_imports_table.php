f5a913a5a2892eac2e9cea629d046e3d61692d84
abdelrahman shrief ali <abdo.shrief270@gmail.com>
2026-01-29 00:09:32 +0200

    feat: Add Contact model authorization policies and create imports and exports database tables.

--- database/migrations/2026_01_28_235517_create_imports_table.php
+++ database/migrations/2026_01_28_235517_create_imports_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -20,7 +19,7 @@ return new class extends Migration
             $table->unsignedInteger('processed_rows')->default(0);
             $table->unsignedInteger('total_rows');
             $table->unsignedInteger('successful_rows')->default(0);
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->morphs('user');
             $table->timestamps();
         });
     }
