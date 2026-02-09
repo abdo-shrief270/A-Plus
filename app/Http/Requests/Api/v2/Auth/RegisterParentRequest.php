b3e2e588805dc4f76c7361164c3b11107a3a8c9b
abdelrahman shrief ali <abdo.shrief270@gmail.com>
2026-02-09 22:25:41 +0200

    Update Validations Register

--- app/Http/Requests/Api/v2/Auth/RegisterParentRequest.php
+++ app/Http/Requests/Api/v2/Auth/RegisterParentRequest.php
@@ -15,7 +15,7 @@ class RegisterParentRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'min:2', 'max:255'],
-            'user_name' => ['required', 'string', 'min:3', 'max:255', 'unique:users,user_name', 'regex:/^[a-zA-Z0-9_-]+$/'],
+            'user_name' => ['nullable', 'string', 'min:3', 'max:255', 'unique:users,user_name', 'regex:/^[a-zA-Z0-9_-]+$/'],
             'email' => ['nullable', 'email', 'max:255', 'unique:users,email'],
             'country_code' => ['required_with:phone', 'string', 'max:5'],
             'phone' => ['required', 'string', 'max:20'],
@@ -29,7 +29,7 @@ class RegisterParentRequest extends FormRequest
         return [
             'name.required' => 'الاسم مطلوب',
             'name.min' => 'الاسم يجب أن يكون حرفين على الأقل',
-            'user_name.required' => 'اسم المستخدم مطلوب',
+//            'user_name.required' => 'اسم المستخدم مطلوب',
             'user_name.unique' => 'اسم المستخدم مستخدم بالفعل',
             'user_name.regex' => 'اسم المستخدم يجب أن يحتوي على أحرف وأرقام فقط',
             'email.email' => 'البريد الإلكتروني غير صالح',
